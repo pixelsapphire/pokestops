7c769309ba0d873c590c7949b1df52400e99fcf0
pixelsapphire <35204889+pixelsapphire@users.noreply.github.com>
2024-05-11 12:40:38 +0200

    Add Sapphire to the game

--- control.js
+++ control.js
@@ -1,4 +1,26 @@
-function toggle() {
-    const toggled = document.querySelector("#visited-switch").checked;
-    document.querySelectorAll(".unvisited").forEach(m => toggled ? m.classList.remove('hidden') : m.classList.add('hidden'));
+let showUnvisited = false;
+let activePlayer = 'Zorie';
+
+function refreshMarkers() {
+    document.querySelectorAll(`.marker.visited-${activePlayer.toLowerCase()}`).forEach(m => {
+        m.style.color = activePlayer === 'Zorie' ? '#4caf50' : '#8566d9';
+        m.style.display = null;
+    });
+    document.querySelectorAll(`.marker:not(.visited-${activePlayer.toLowerCase()})`).forEach(m => {
+        m.style.color = 'red';
+        m.style.display = showUnvisited ? null : 'none';
+    });
+}
+
+function toggleUnvisited() {
+    showUnvisited = document.querySelector("#visited-switch").checked;
+    refreshMarkers();
+}
+
+function togglePlayer() {
+    activePlayer = document.querySelector("#player-switch").checked ? 'Zorie' : 'Sapphire';
+    document.querySelector('#nickname').innerHTML = activePlayer;
+    const percentage = document.querySelector('#exploration-percentage');
+    percentage.innerHTML = percentage.getAttribute(`data-${activePlayer.toLowerCase()}`);
+    refreshMarkers();
 }
